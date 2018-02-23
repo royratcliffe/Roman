75dfee5994c184a82421e292e295aeafc2cac6a4
Roy Ratcliffe <royratcliffe@metsoftpro.com>
2018-02-23 09:18:59 +0000

    Draw lines, stroke black

--- Roman/MainViewModel.fs
+++ Roman/MainViewModel.fs
@@ -2,6 +2,18 @@
 
 open ViewModule
 open ViewModule.FSharp
+open System.Collections.ObjectModel
+
+type Point = { X: float; Y: float }
+type Line = { Start: Point; End: Point }
 
 type MainViewModel () =
     inherit ViewModelBase ()
+
+    let lines = ObservableCollection<Line> ()
+
+    do
+        { Start = { X = 0.0; Y = 0.0 }; End = { X = 100.0; Y = 100.0 } }
+        |> lines.Add
+
+    member __.Lines = lines
